715995f645500d637107de68fc18c94a35b63bdb
Maciej Malecki <malecki.maciej@gmail.com>
2021-04-22 16:15:48 +0200

    asciidoc generation

---
+++ .circleci/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
+
+ls -al
+
+git stash
+git remote rm origin
+git remote add origin https://maciejmalecki:"$GITHUB_TOKEN"@github.com/c64lib/gradle-retro-assembler-plugin.git
+
+ls -al
+
+find . -maxdepth 1 ! -name 'doc/out' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
+mv doc/out/* .
+rm -R doc/
+
+ls -al
+
+git add -fA
+git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
+git push -f origin master:gh-pages
+
+echo "deployed successfully"
---
+++ doc/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#bundle exec asciidoctor -D out -r asciidoctor-diagram -r asciidoctor-kroki index.adoc
+bundle exec asciidoctor -D out -r asciidoctor-kroki index.adoc
