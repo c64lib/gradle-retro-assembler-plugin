113d746d7e9b59b2067feac7268b8dee23a3ca5e
Maciej Małecki <maciej.malecki@capgemini.com>
2021-08-22 21:39:31 +0200

    Bump to 1.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ java {
 allprojects {
 
     group = "com.github.c64lib"
-    version = "1.4.1-SNAPSHOT"
+    version = "1.4.1"
 
     if (project.hasProperty(tagPropertyName)) {
         version = project.property(tagPropertyName) ?: version
