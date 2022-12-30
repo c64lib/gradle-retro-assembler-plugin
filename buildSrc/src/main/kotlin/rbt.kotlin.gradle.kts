5741c0e3abceee3b30d8d58b436ee20152947a0c
Maciej Malecki <malecki.maciej@gmail.com>
2022-12-31 00:47:30 +0100

    Enabling spotless

--- buildSrc/src/main/kotlin/rbt.kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/rbt.kotlin.gradle.kts
@@ -3,10 +3,10 @@ plugins {
   id("com.diffplug.spotless")
 }
 
-//java {
-//  sourceCompatibility = JavaVersion.VERSION_11
-//  targetCompatibility = JavaVersion.VERSION_11
-//}
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
   kotlinOptions {
