dd7c23c91668242cdb1abe466258129f9720e791
Maciej Małecki <malecki.maciej@gmail.com>
2023-06-10 23:10:57 +0200

    prepare for 1.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 allprojects {
 
     group = "com.github.c64lib"
-    version = "1.7.2-SNAPSHOT"
+    version = "1.7.2"
 
     if (project.hasProperty(tagPropertyName)) {
         version = project.property(tagPropertyName) ?: version
