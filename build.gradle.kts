67e442b844153c487d0d3e8dc3cc156860a8bce0
Maciej Małecki <malecki.maciej@gmail.com>
2023-06-10 22:53:22 +0200

    handling RGBA PNGs

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 allprojects {
 
     group = "com.github.c64lib"
-    version = "1.7.1"
+    version = "1.7.2-SNAPSHOT"
 
     if (project.hasProperty(tagPropertyName)) {
         version = project.property(tagPropertyName) ?: version
